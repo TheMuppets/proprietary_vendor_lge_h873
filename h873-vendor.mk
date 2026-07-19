4851ff7385b3680cc365a41e02cffaa6edb2245a
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-19 17:46:14 -0400

    Deduplicate bcm cal from vendor
    Change-Id: I1a6b70531f075506f25316777afb6f7fcab48b37

--- h873-vendor.mk
+++ h873-vendor.mk
@@ -10,7 +10,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/h873/proprietary/vendor/firmware/fw_bcmdhd_apsta.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fw_bcmdhd_apsta.bin \
     vendor/lge/h873/proprietary/vendor/firmware/fw_bcmdhd_mfg.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fw_bcmdhd_mfg.bin \
     vendor/lge/h873/proprietary/vendor/etc/wifi/4359_lg.clm_blob:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/4359_lg.clm_blob \
-    vendor/lge/h873/proprietary/vendor/etc/wifi/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal \
     vendor/lge/h873/proprietary/vendor/firmware/BCM43xx.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM43xx.hcd \
     vendor/lge/h873/proprietary/vendor/firmware/a530_gpmu.fw2:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_gpmu.fw2 \
     vendor/lge/h873/proprietary/vendor/firmware/a530_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pfp.fw \
